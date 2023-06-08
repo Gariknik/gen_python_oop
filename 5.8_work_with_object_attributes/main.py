8c1704c2c0cc4c09e7b61dea37862ca28156c3b9
Gariknik <nikitenkoivi@gmail.com>
2023-06-08 13:15:53 +0300

    solve work_with_object_attributes task part 3 08/06/2023

--- 5.8_work_with_object_attributes/main.py
+++ 5.8_work_with_object_attributes/main.py
@@ -291,6 +291,204 @@ class NonNegativeObject:
             value = value * (-1)
         super().__setattr__(key, value)
 
+#6
+"""
+
+Класс AttrsNumberObject
+Реализуйте класс AttrsNumberObject. При создании экземпляра класс должен принимать произвольное количество именованных аргументов. Все передаваемые аргументы должны устанавливаться создаваемому экземпляру в качестве атрибутов.
+
+Экземпляр класса AttrsNumberObject должен иметь один атрибут:
+
+attrs_num — количество атрибутов, которыми обладает экземпляр класса AttrsNumberObject на данный момент, включая сам атрибут attrs_num
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализации класса AttrsNumberObject нет, она может быть произвольной.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+music_group = AttrsNumberObject(name='Silent Poets', genre='acid jazz')
+
+print(music_group.attrs_num)
+Sample Output 1:
+
+3
+Sample Input 2:
+
+music_group = AttrsNumberObject()
+
+print(music_group.attrs_num)
+Sample Output 2:
+
+1
+Sample Input 3:
+
+music_group = AttrsNumberObject(name='Woodkid', genre='pop')
+
+print(music_group.attrs_num)
+music_group.country = 'France'
+print(music_group.attrs_num)
+Sample Output 3:
+
+3
+4
+Sample Input 4:
+
+music_group = AttrsNumberObject(name='Alexandra Savior', genre='dream pop')
+
+print(music_group.attrs_num)
+del music_group.genre
+print(music_group.attrs_num)
+Sample Output 4:
+
+3
+2
+
+"""
+
+class AttrsNumberObject:
+    def __init__(self, **kwargs):
+        self.attrs_num = 0
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+    
+    def __setattr__(self, attr, value):
+        super().__setattr__(attr, value)
+        self.__dict__['attrs_num'] += 1
+        
+    def __delattr__(self, attr):
+        super().__delattr__(attr)
+        self.__dict__['attrs_num'] -= 1
+
+
+#7
+"""
+
+Класс Const
+Реализуйте класс Const. При создании экземпляра класс должен принимать произвольное количество именованных аргументов. Все передаваемые аргументы должны устанавливаться создаваемому экземпляру в качестве атрибутов.
+
+Класс Const должен разрешать устанавливать атрибуты своим экземплярам и получать их значения, но не разрешать изменять значения этих атрибутов, а также удалять их. При попытке изменить значение атрибута должно возбуждаться исключение AttributeError с текстом:
+
+Изменение значения атрибута невозможно
+При попытке удалить атрибут должно возбуждаться исключение AttributeError с текстом:
+
+Удаление атрибута невозможно
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализации класса Const нет, она может быть произвольной.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+videogame = Const(name='Cuphead')
+
+videogame.developer = 'Studio MDHR'
+print(videogame.name)
+print(videogame.developer)
+Sample Output 1:
+
+Cuphead
+Studio MDHR
+Sample Input 2:
+
+videogame = Const(name='Dicso Elysium')
+
+try:
+    videogame.name = 'Half-Life: Alyx'
+except AttributeError as e:
+    print(e)
+Sample Output 2:
+
+Изменение значения атрибута невозможно
+Sample Input 3:
+
+videogame = Const(name='The Last of Us')
+
+try:
+    del videogame.name
+except AttributeError as e:
+    print(e)
+Sample Output 3:
+
+Удаление атрибута невозможно
+
+"""
+
+class Const:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+    
+    def __setattr__(self, attr, value):
+        if attr in self.__dict__:
+            raise AttributeError ('Изменение значения атрибута невозможно')
+        super().__setattr__(attr, value)
+    
+    def __delattr__(self, attr):
+        raise AttributeError ('Удаление атрибута невозможно')
+
+
+#8
+"""
+
+Класс ProtectedObject
+Будем считать атрибут защищенным, если его имя начинается с символа нижнего подчеркивания (_). Например, _password, __email и __dict__.
+
+Реализуйте класс ProtectedObject. При создании экземпляра класс должен принимать произвольное количество именованных аргументов. Все передаваемые аргументы должны устанавливаться создаваемому экземпляру в качестве атрибутов.
+
+Класс ProtectedObject должен запрещать получать и изменять значения защищенных атрибутов своих экземпляров, а также удалять эти атрибуты. При попытке получить или изменить значение защищенного атрибута, а также попытке удалить атрибут, должно возбуждаться исключение AttributeError с текстом:
+
+Доступ к защищенному атрибуту невозможен
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализации класса ProtectedObject нет, она может быть произвольной.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input:
+
+user = ProtectedObject(login='PG_kamiya', _password='alreadybanned')
+
+try:
+    print(user.login)
+    print(user._password)
+except AttributeError as e:
+    print(e)
+Sample Output:
+
+PG_kamiya
+Доступ к защищенному атрибуту невозможен
+
+
+"""
+
+class ProtectedObject:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            super().__setattr__(key, value)
+
+    def __getattribute__(self, attr):
+        if attr[0] == '_':
+            raise AttributeError ('Доступ к защищенному атрибуту невозможен')
+        return super().__getattribute__(attr)
+    
+    def __setattr__(self, attr, value):
+        if attr[0] == '_':
+            raise AttributeError ('Доступ к защищенному атрибуту невозможен')
+        super().__setattr__(attr, value)
+    
+    def __delattr__(self, attr):
+        if attr[0] == '_':
+            raise AttributeError ('Доступ к защищенному атрибуту невозможен')
+        super().__delattr__(attr)   
+
 
 
 if __name__ == '__main__':
@@ -328,3 +526,24 @@ if __name__ == '__main__':
     print(point.y)
     print(point.z)
     print(point.color)
+
+    music_group = AttrsNumberObject(name='Woodkid', genre='pop')
+
+    print(music_group.attrs_num)
+    music_group.country = 'France'
+    print(music_group.attrs_num)
+
+    videogame = Const(name='Dicso Elysium')
+
+    try:
+        videogame.name = 'Half-Life: Alyx'
+    except AttributeError as e:
+        print(e)
+
+    user = ProtectedObject(login='PG_kamiya', _password='alreadybanned')
+
+    try:
+        print(user.login)
+        print(user._password)
+    except AttributeError as e:
+        print(e)
