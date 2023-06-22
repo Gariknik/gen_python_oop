ef7eba8abf9d48461cc59439a9db755958133fe6
Gariknik <nikitenkoivi@gmail.com>
2023-06-22 21:07:47 +0300

    solve susequence tasks 22/06/2023

--- 6.2_subsequence/main.py
+++ 6.2_subsequence/main.py
@@ -75,6 +75,81 @@ class ReversedSequence:
     
     def __getitem__(self, key):
         return self.sequence[~key]
+    
+
+
+#2
+"""
+
+Класс SparseArray
+Разреженный массив (список) — абстрактное представление обычного массива (списка), в котором данные представлены не непрерывно, а фрагментарно: большинство его элементов принимает одно и то же значение по умолчанию, обычно 0 или None. В разреженном массиве возможен доступ к неопределенным элементам, в этом случае массив вернет некоторое значение по умолчанию.
+
+Реализуйте класс SparseArray, описывающий разреженный массив. При создании экземпляра класс должен принимать один аргумент:
+
+default — значение по умолчанию для неопределенных элементов разреженного массива
+Экземпляр класса SparseArray должен позволять получать и изменять значения своих элементов с помощью индексов. При попытке получить значение элемента по несуществующему индексу должно быть возвращено значение по умолчанию.
+
+Примечание 1. Гарантируется, что при доступе к элементам используются только неотрицательные индексы.
+
+Примечание 2. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 3. Никаких ограничений касательно реализации класса SparseArray нет, она может быть произвольной.
+
+Примечание 4. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+array = SparseArray(0)
+
+array[5] = 1000
+array[12] = 1001
+
+print(array[5])
+print(array[12])
+print(array[13])
+Sample Output 1:
+
+1000
+1001
+0
+Sample Input 2:
+
+array = SparseArray(None)
+
+array[0] = 'Timur'
+array[1] = 'Arthur'
+
+print(array[0])
+print(array[1])
+print(array[2])
+Sample Output 2:
+
+Timur
+Arthur
+None
+
+"""
+
+class SparseArray:
+    def __init__(self, default) -> None:
+        self.default = default
+        self.lst = []
+
+    def __getitem__(self, key):
+        if key >= len(self.lst):
+            for _  in range(len(self.lst), key+1):
+                self.lst += [self.default]
+        return self.lst[key]
+    
+    def __setitem__(self, key, value):
+        if key >= len(self.lst):
+            for _  in range(len(self.lst), key+1):
+                self.lst += [self.default]
+        self.lst[key] = value
+
+
 
 
 if __name__ == '__main__':
@@ -84,3 +159,13 @@ if __name__ == '__main__':
     print(reversed_numbers[0])
     numbers.append(6)
     print(reversed_numbers[0])
+
+
+    array = SparseArray(0)
+
+    array[5] = 1000
+    array[12] = 1001
+
+    print(array[5])
+    print(array[12])
+    print(array[13])
