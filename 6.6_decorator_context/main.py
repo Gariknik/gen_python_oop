664b56c1dc09a78d2b29dd6dbde5c1aa7720eb3e
Gariknik <nikitenkoivi@gmail.com>
2023-07-17 11:08:32 +0300

    solve decorator_context task 17/07/2023

--- 6.6_decorator_context/main.py
+++ 6.6_decorator_context/main.py
@@ -109,10 +109,66 @@ def reversed_print():
     finally:
         sys.stdout.write = s
 
+#3
+"""
+
+Контекстный менеджер safe_write
+Реализуйте контекстный менеджер safe_write с помощью декоратора @contextmanager, который принимает один аргумент:
+
+filename — имя файла
+Контекстный менеджер должен открывать файл с именем filename в режиме w и позволять выполнять с ним соответствующие операции. Причем если во время записи в файл было возбуждено какое-либо исключение, контекстный менеджер должен поглотить его, отменить все выполненные ранее записи в файл, если они были, вернуть файл в исходное состояние и проинформировать о возбужденном исключении выводом следующего текста:
+
+Во время записи в файл было возбуждено исключение <тип исключения>
+Примечание 1. Наглядные примеры использования контекстного менеджера safe_write продемонстрированы в тестовых данных.
+
+Примечание 2. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный контекстный менеджер используется только с корректными данными.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+with safe_write('undertale.txt') as file:
+    file.write('Тень от руин нависает над вами, наполняя вас решительностью')
+    
+with open('undertale.txt') as file:
+    print(file.read())
+Sample Output 1:
+
+Тень от руин нависает над вами, наполняя вас решительностью
+Sample Input 2:
+
+with safe_write('undertale.txt') as file:
+    file.write('Тень от руин нависает над вами, наполняя вас решительностью\n')
+    
+with safe_write('undertale.txt') as file:
+    print('Под весёлый шорох листвы вы наполняетесь решительностью', file=file)
+    raise ValueError
+
+with open('undertale.txt') as file:
+    print(file.read())
+Sample Output 2:
+
+Во время записи в файл было возбуждено исключение ValueError
+Тень от руин нависает над вами, наполняя вас решительностью
+
+"""
 
 
+from contextlib import contextmanager, ExitStack
 
-
+@contextmanager
+def safe_write(filename):
+    stack = ExitStack()
+    file = stack.enter_context(open(filename, 'w'))
+    try:
+        yield file
+    except Exception as e:
+        print(f'Во время записи в файл было возбуждено исключение {type(e).__name__}')  
+    finally:
+        stack.close()
+        
 
 
 if __name__ == '__main__':
@@ -127,3 +183,14 @@ if __name__ == '__main__':
         print('Вывод внутри блока with')
 
     print('Вывод вне блока with')
+
+
+    with safe_write('undertale.txt') as file:
+        file.write('Тень от руин нависает над вами, наполняя вас решительностью\n')
+        
+    with safe_write('undertale.txt') as file:
+        print('Под весёлый шорох листвы вы наполняетесь решительностью', file=file)
+        raise ValueError
+
+    with open('undertale.txt') as file:
+        print(file.read())
