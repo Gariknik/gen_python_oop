b1db2afb4d4b176edc042b7b9c81c3050b8654bc
Gariknik <nikitenkoivi@gmail.com>
2023-07-19 09:32:18 +0300

    solve context_maneger 19/07/2023

--- 6.5_context_maneger_protocol/main.py
+++ 6.5_context_maneger_protocol/main.py
@@ -978,34 +978,56 @@ Sample Output 3:
 
 """
 class TreeBuilder:
-    struct = []
-    level = 0
-    index = 0
-    
     def __init__(self):
-        pass
-    
-    def add(self, elem):
-        if TreeBuilder.level == 0:
-            TreeBuilder.struct += [elem]
-            TreeBuilder.index += 1
-        TreeBuilder.struct[TreeBuilder.index] += [elem]
-    
-    def structure(self):
-        return TreeBuilder.struct
-    
-    def __enter__(self):
-        TreeBuilder.level += 1
+        self.knots = [[]]
         
-        TreeBuilder.struct += [[]]
-        return self
+    def __enter__(self):
+        self.knots.append([])
+        
+    def __exit__(self, *args, **kwargs):
+        if self.knots[-1]:
+            self.knots[-2].append(self.knots[-1])
+        self.knots.pop()
     
-    def __exit__(self, exc_type, exc_value, traceback):
-        TreeBuilder.level -= 1
-        TreeBuilder.index += 1
+    def add(self, value):
+        self.knots[-1].append(value)
+        
+    def structure(self):
+        return self.knots[-1]
+    
+    """
+    
+    class TreeBuilder:
+    def __init__(self):
+        self._tree = []
+        self._t = {}
+        self.level = 0
 
-        TreeBuilder.struct += []
-        return True
+    def __enter__(self):
+        self._t.setdefault(self.level, []).append([])
+        self.level += 1
+        self._t[self.level] = self._t[self.level - 1][-1]
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.level -= 1
+        if not self._t[self.level][-1]:
+            self._t[self.level].pop()
+        if self.level == 0:
+            self._tree.extend(self._t[self.level])
+            self._t.clear()
+
+    def add(self, item):
+        if self.level == 0:
+            self._tree.append(item)
+        else:
+            self._t[self.level].append(item)
+
+    def structure(self):
+        return self._tree
+    
+    
+    """
 
 
 
