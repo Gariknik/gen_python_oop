c74eb9275f61dbd0fee3226b265b8a13209436cf
Gariknik <nikitenkoivi@gmail.com>
2023-08-10 08:38:44 +0300

    solve tasks context maneger protocol 10/08/2023

--- 6.5_context_maneger_protocol/main.py
+++ 6.5_context_maneger_protocol/main.py
@@ -977,27 +977,10 @@ Sample Output 3:
 
 
 """
+
+    
+    
 class TreeBuilder:
-    def __init__(self):
-        self.knots = [[]]
-        
-    def __enter__(self):
-        self.knots.append([])
-        
-    def __exit__(self, *args, **kwargs):
-        if self.knots[-1]:
-            self.knots[-2].append(self.knots[-1])
-        self.knots.pop()
-    
-    def add(self, value):
-        self.knots[-1].append(value)
-        
-    def structure(self):
-        return self.knots[-1]
-    
-    """
-    
-    class TreeBuilder:
     def __init__(self):
         self._tree = []
         self._t = {}
@@ -1027,7 +1010,7 @@ class TreeBuilder:
         return self._tree
     
     
-    """
+    
 
 """
 конец урока
