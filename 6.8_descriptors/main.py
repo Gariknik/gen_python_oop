b39b32ad8c3ddbdd12c6beab5002bf5f989ef784
Gariknik <nikitenkoivi@gmail.com>
2023-08-11 08:50:59 +0300

    solve deskriptors task 11/08/2023

--- 6.8_descriptors/main.py
+++ 6.8_descriptors/main.py
@@ -83,6 +83,86 @@ class NonKeyword:
     def __delete__(self, obj):
         del obj.__dict__[self._attr]
 
+#2
+"""
+Класс NonNegativeInteger
+Реализуйте класс NonNegativeInteger, описывающий дескриптор, который проверяет, что устанавливаемое или изменяемое значение атрибута является неотрицательным целым числом. При создании экземпляра класс должен принимать два аргумента в следующем порядке:
+
+name — имя атрибута, за которым будет закреплен дескриптор
+default — значение по умолчанию. Если не передан, имеет значение None
+При обращении к атрибуту дескриптор должен возвращать значение этого атрибута, если оно установлено. Если значение не установлено, должно возвращаться значение по умолчанию, указанное при создании дескриптора. Если значение по умолчанию равняется None, должно быть возбуждено исключение AttributeError с текстом:
+
+Атрибут не найден
+При установке или изменении значения атрибута дескриптор должен проверять, является ли это значение неотрицательным целым числом. Если значение не является неотрицательным целым числом, должно быть возбуждено исключение ValueError с текстом:
+
+Некорректное значение
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализации класса NonNegativeInteger нет, она может быть произвольной.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+class Student:
+    score = NonNegativeInteger('score')
+
+student = Student()
+student.score = 90
+
+print(student.score)
+Sample Output 1:
+
+90
+Sample Input 2:
+
+class Student:
+    score = NonNegativeInteger('score', 0)
+
+student = Student()
+
+print(student.score)
+student.score = 0
+print(student.score)
+Sample Output 2:
+
+0
+0
+Sample Input 3:
+
+class Student:
+    score = NonNegativeInteger('score')
+
+student = Student()
+
+try:
+    print(student.score)
+except AttributeError as e:
+    print(e)
+Sample Output 3:
+
+Атрибут не найден
+Sample Input 4:
+
+class Student:
+    score = NonNegativeInteger('score')
+
+student = Student()
+
+try:
+    student.score = -90
+except ValueError as e:
+    print(e)
+Sample Output 4:
+
+Некорректное значение
+
+
+"""
+
+
 
 
 if __name__ == "__main__":
