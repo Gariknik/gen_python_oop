b7a3d0f52c54fc183ba80c3ba7665a9bf7b3f841
Gariknik <nikitenkoivi@gmail.com>
2023-08-14 09:33:14 +0300

    solve descriptors tasks 14/08/2023

--- 6.8_descriptors/main.py
+++ 6.8_descriptors/main.py
@@ -244,6 +244,27 @@ Gwen
 
 
 """
+class MaxCallsException(Exception):
+    pass
+
+class LimitedTakes:
+    def __set_name__(self, cls, attr):
+        self._attr = attr
+
+    def __init__(self, times):
+        self._times = times
+
+    def __get__(self, obj, cls):
+        if self._attr not in obj.__dict__:
+            raise AttributeError('Атрибут не найден')
+        if self._times > 0:
+            self._times -= 1
+            return obj.__dict__[self._attr]
+        else:
+            raise MaxCallsException('Превышено количество доступных обращений')
+
+    def __set__(self, obj, value):
+        obj.__dict__[self._attr] = value
 
 
 
@@ -277,3 +298,19 @@ if __name__ == "__main__":
     print(student.score)
     student.score = 0
     print(student.score)
+
+
+    class Student:
+        name = LimitedTakes(3)
+
+    student = Student()
+    student.name = 'Gwen'
+
+    print(student.name)
+    print(student.name)
+    print(student.name)
+
+    try:
+        print(student.name)
+    except MaxCallsException as e:
+        print(e)
