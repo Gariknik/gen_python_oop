4929c0c7f71a8fd9a847864c0d3514282e7c42ed
Gariknik <nikitenkoivi@gmail.com>
2023-08-15 09:26:49 +0300

    solve descriptors tasks 15/08/2023

--- 6.8_descriptors/main.py
+++ 6.8_descriptors/main.py
@@ -266,7 +266,91 @@ class LimitedTakes:
     def __set__(self, obj, value):
         obj.__dict__[self._attr] = value
 
+#4
+"""
 
+Класс TypeChecked
+Реализуйте класс TypeChecked, описывающий дескриптор, который проверяет, что устанавливаемое или изменяемое значение атрибута принадлежит определенному типу данных. При создании экземпляра класс должен принимать произвольное количество позиционных аргументов, каждый из которых является типом данных.
+
+Дескриптор должен закрепляться за атрибутом, имеющим то же имя, что и переменная, которой присваивается дескриптор.
+
+При обращении к атрибуту дескриптор должен возвращать значение этого атрибута, если оно установлено. Если значение атрибута не установлено, должно быть возбуждено исключение AttributeError с текстом:
+
+Атрибут не найден
+При установке или изменении значения атрибута дескриптор должен проверять, принадлежит ли это значение одному из типов, указанных при создании дескриптора. Если значение не принадлежит ни одному из типов, должно быть возбуждено исключение TypeError с текстом:
+
+Некорректное значение
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализации класса TypeChecked нет, она может быть произвольной.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+class Student:
+    name = TypeChecked(str)
+
+student = Student()
+student.name = 'Mary'
+
+print(student.name)
+Sample Output 1:
+
+Mary
+Sample Input 2:
+
+class Student:
+    name = TypeChecked(str)
+
+student = Student()
+
+try:
+    print(student.name)
+except AttributeError as e:
+    print(e)
+Sample Output 2:
+
+Атрибут не найден
+Sample Input 3:
+
+class Student:
+    name = TypeChecked(str)
+
+student = Student()
+student.name = 'Mary'
+
+try:
+    student.name = 99
+except TypeError as e:
+    print(e)
+
+print(student.name)
+Sample Output 3:
+
+Некорректное значение
+Mary
+Sample Input 4:
+
+class Student:
+    age = TypeChecked(int, float)
+
+student = Student()
+
+student.age = 18
+print(student.age)
+
+student.age = 18.5
+print(student.age)
+Sample Output 4:
+
+18
+18.5
+
+
+"""
 
 if __name__ == "__main__":
     
