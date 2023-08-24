f66388bec9491dac212973ff7b86ffc712854932
Gariknik <nikitenkoivi@gmail.com>
2023-08-24 08:36:04 +0300

    solve inheritance_part_1 tasks 24/08/2023

--- 7.1_inheritance_part_1/main.py
+++ 7.1_inheritance_part_1/main.py
@@ -207,6 +207,16 @@ True
 
 
 """
+class User:
+    def __init__(self, name):
+        self.name = name
+
+    def skip_ads(self):
+        return False
+
+class PremiumUser(User):
+    def skip_ads(self):
+        return True
 
 
 if __name__ == '__main__':
@@ -224,3 +234,10 @@ if __name__ == '__main__':
     print(issubclass(Bird, Animal))
     print(issubclass(FlyingBird, Animal))
     print(issubclass(FlyingBird, Bird))
+
+
+    user = User('Arthur')
+    premium_user = PremiumUser('Arthur')
+
+    print(user.skip_ads())
+    print(premium_user.skip_ads())
