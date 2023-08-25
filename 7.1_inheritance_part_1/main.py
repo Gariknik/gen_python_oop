a643d4a9d7a4ccba74ac706d0b965c7490ab9d8c
Gariknik <nikitenkoivi@gmail.com>
2023-08-25 08:29:28 +0300

    solve inheritance part 1 25/08/2023

--- 7.1_inheritance_part_1/main.py
+++ 7.1_inheritance_part_1/main.py
@@ -218,6 +218,71 @@ class PremiumUser(User):
     def skip_ads(self):
         return True
 
+#5
+"""
+
+Классы Validator и NumberValidator
+Реализуйте класс Validator, описывающий объект, проверяющий значение на корректность. При создании экземпляра класс должен принимать один аргумент:
+
+obj — произвольный объект
+Класс Validator должен иметь один метод экземпляра:
+
+is_valid() — пустой метод, всегда возвращающий значение None
+Также реализуйте класс NumberValidator, наследника класса Validator, описывающий объект, проверяющий значение на принадлежность типу int или float. Процесс создания экземпляра класса NumberValidator должен совпадать с процессом создания экземпляра класса Validator.
+
+Класс NumberValidator должен иметь один метод экземпляра:
+
+is_valid() — метод, возвращающий True, если значение переданное в инициализатор принадлежит типу int или float, или False в противном случае
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализации классов нет, она может быть произвольной.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+print(issubclass(NumberValidator, Validator))
+Sample Output 1:
+
+True
+Sample Input 2:
+
+validator1 = Validator('beegeek')
+validator2 = Validator(1)
+validator3 = Validator(1.1)
+
+print(validator1.is_valid())
+print(validator2.is_valid())
+print(validator3.is_valid())
+Sample Output 2:
+
+None
+None
+None
+Sample Input 3:
+
+validator1 = NumberValidator('beegeek')
+validator2 = NumberValidator(1)
+validator3 = NumberValidator(1.1)
+
+print(validator1.is_valid())
+print(validator2.is_valid())
+print(validator3.is_valid())
+Sample Output 3:
+
+False
+True
+True
+
+"""
+
+class Validator:
+    pass
+
+class NumberValidator(Validator):
+    pass
 
 if __name__ == '__main__':
     print(issubclass(Car, LandVehicle))
