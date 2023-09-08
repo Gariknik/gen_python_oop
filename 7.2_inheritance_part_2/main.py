2e2545a2935a9c852a15e8f6252db9812129a327
Gariknik <nikitenkoivi@gmail.com>
2023-09-08 08:09:27 +0300

    solve inheritance part 3 08/09/2023

--- 7.2_inheritance_part_2/main.py
+++ 7.2_inheritance_part_2/main.py
@@ -701,6 +701,49 @@ False
 """
 
 
+
+
+#8
+"""
+
+Класс UpperPrintString
+Реализуйте класс UpperPrintString, наследника класса str, описывающий строку. Процесс создания экземпляра класса UpperPrintString должен совпадать с процессом создания экземпляра класса str.
+
+Экземпляр класса UpperPrintString должен иметь следующее неформальное строковое представление:
+
+<значение строки в верхнем регистре>
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализации класса UpperPrintString нет, она может быть произвольной.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+s1 = UpperPrintString('beegeek')
+s2 = UpperPrintString('BeeGeek')
+
+print(s1)
+print(s2)
+Sample Output 1:
+
+BEEGEEK
+BEEGEEK
+Sample Input 2:
+
+s = UpperPrintString('beegeek')
+print(list(s))
+Sample Output 2:
+
+['b', 'e', 'e', 'g', 'e', 'e', 'k']
+
+"""
+class UpperPrintString(str):
+    def __str__(self):
+        return self.upper()
+
 if __name__ == '__main__':
     print(SilverPlan.can_stream)
     print(SilverPlan.can_download)
@@ -777,3 +820,10 @@ if __name__ == '__main__':
     print(summator1.total(3))    # 1 + 2 + 3
     print(summator2.total(3))    # 1 + 4 + 9
     print(summator3.total(3))    # 1 + 8 + 27
+
+
+    s1 = UpperPrintString('beegeek')
+    s2 = UpperPrintString('BeeGeek')
+
+    print(s1)
+    print(s2)
