1bf27ff04412ae2ce99effdb753836e37e0575b5
Gariknik <nikitenkoivi@gmail.com>
2023-09-12 09:10:27 +0300

    solve inheritance 12/09/2023

--- 7.2_inheritance_part_2/main.py
+++ 7.2_inheritance_part_2/main.py
@@ -873,6 +873,60 @@ class FuzzyString(str):
         if not isinstance(other, (FuzzyString, str)) or not isinstance(self, (FuzzyString, str)):
             return NotImplemented
         return other.lower() in self.lower()  
+    
+#11
+"""
+
+Класс TitledText
+Реализуйте класс TitledText, наследника класса str, который описывает текст, имеющий заголовок. При создании экземпляра класс должен принимать два аргумента в следующем порядке:
+
+content — текст
+text_title — заголовок текста
+Класс TitleText должен иметь один метод экземпляра:
+
+title() — метод, возвращающий заголовок текста
+Примечание 1. Значением экземпляра класса TitledText должен быть именно текст, а не заголовок текста или текст вместе с заголовком.
+
+Примечание 2. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 3. Никаких ограничений касательно реализации класса TitledText нет, она может быть произвольной.
+
+Примечание 4. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+titled = TitledText('Сreate a class Soda', 'Homework')
+
+print(titled)
+print(titled.title())
+print(issubclass(TitledText, str))
+Sample Output 1:
+
+Сreate a class Soda
+Homework
+True
+Sample Input 2:
+
+titled1 = TitledText('Сreate a class Soda', 'Homework')
+titled2 = TitledText('Сreate a class Soda', 'Exam')
+
+print(titled1 == titled2)
+Sample Output 2:
+
+True
+
+"""
+class TitledText(str):
+    def __new__(cls, content, text_title):
+        instance = super().__new__(cls, content)
+        instance.text_title = text_title
+        return instance
+   
+    def title(self):
+        return self.text_title
+    
 
 
 
@@ -969,3 +1023,10 @@ if __name__ == '__main__':
 
     print(s2 in s1)
     print(s1 in s2)
+
+
+    titled = TitledText('Сreate a class Soda', 'Homework')
+
+    print(titled)
+    print(titled.title())
+    print(issubclass(TitledText, str))
