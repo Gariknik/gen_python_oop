92ffb9eab9b4a5fa99a2ce8d02af130ef8b68d2f
Gariknik <nikitenkoivi@gmail.com>
2023-11-23 10:39:34 +0200

    solve tasks 23/11/2023

--- 7.5_abstract_classes/main.py
+++ 7.5_abstract_classes/main.py
@@ -336,8 +336,58 @@ if __name__ == '__main__':
                 return result
             self.num_1, self.num_2 = self.num_2, self.num_1 + self.num_2
             return result
+        
     fibonacci_iter = FibonacciIterator(7)
 
     for number in fibonacci_iter:
         print(number)
 
+
+    class Book:
+        def __init__(self, title, pages):
+            self.title = title
+            self.pages = pages
+
+
+    class Library:
+        def __init__(self):
+            self.books = []
+
+        def add_book(self, book):
+            self.books.append(book)
+
+        def __iter__(self):
+            new_lib = []
+            for book in self.books:
+                new_lib += book.pages
+            return LibraryIterator(new_lib) # тут определите, что будете передавать итератору
+
+
+    class LibraryIterator:
+        def __init__(self, book):
+            self.book = book
+            self.index = 0
+              
+        def __next__(self):
+            curr_index = self.index
+            if curr_index >= len(self.book):
+                raise StopIteration
+            self.index += 1
+            return self.book[curr_index]
+
+            
+
+
+    # Пример использования
+    book1 = Book("Book 1", ["Page 1", "Page 2", "Page 3", "Page 4"])
+    book2 = Book("Book 2", ["Page A", "Page B", "Page C"])
+    book3 = Book("Book 3", ["Chapter 1", "Chapter 2"])
+
+    library = Library()
+    library.add_book(book1)
+    library.add_book(book2)
+    library.add_book(book3)
+
+    # Используем вложенный итератор для обхода страниц в библиотеке
+    for page in library:
+        print(page)
