1490ee4d32ce632cfd37a3fc4169e46d217bd568
Gariknik <nikitenkoivi@gmail.com>
2023-11-27 11:19:33 +0200

    solve abstract classes tasks 27/11/2023

--- 7.5_abstract_classes/main.py
+++ 7.5_abstract_classes/main.py
@@ -189,6 +189,227 @@ class Knight(ChessPiece):
     def can_move(self, horizontal_to, vertical_to):
         return (self.hor_coords[horizontal_to] - self.hor_coords[self.horizontal]) ** 2\
               + (self.vert_coords[vertical_to] - self.vert_coords[self.vertical]) ** 2 == 5
+    
+
+
+"""
+Функции is_iterator() и is_iterable()
+1. Реализуйте функцию is_iterable(), которая принимает один аргумент:
+
+obj — произвольный объект
+Функция должна возвращать True, если объект obj является итерируемым объектом, или False в противном случае.
+
+2. Также реализуйте функцию is_iterator(), которая принимает один аргумент:
+
+obj — произвольный объект
+Функция должна возвращать True, если объект obj является итератором, или False в противном случае.
+
+Примечание 1. В тестирующую систему сдайте программу, содержащую только необходимые функции, но не код, вызывающий их.
+
+Примечание 2. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+print(is_iterable(123))
+print(is_iterable([1, 2, 3]))
+print(is_iterable((1, 2, 3)))
+print(is_iterable('123'))
+print(is_iterable(iter('123')))
+print(is_iterable(map(int, '123')))
+Sample Output 1:
+
+False
+True
+True
+True
+True
+True
+Sample Input 2:
+
+print(is_iterator(123))
+print(is_iterator([1, 2, 3]))
+print(is_iterator((1, 2, 3)))
+print(is_iterator('123'))
+print(is_iterator(iter('123')))
+print(is_iterator(map(int, '123')))
+Sample Output 2:
+
+False
+False
+False
+False
+True
+True
+"""
+
+def is_iterable(obj):
+    return hasattr(obj, '__iter__') or hasattr(obj, '__getitem__')
+def is_iterator(obj):
+    return hasattr(obj, '__iter__') and hasattr(obj, '__next__')
+
+
+"""
+Классы Validator, Number и String
+1. Реализуйте абстрактный класс Validator, описывающий дескриптор, который проверяет, что устанавливаемое или изменяемое значение является корректным. При создании экземпляра класс не должен принимать никаких аргументов.
+
+Дескриптор должен закрепляться за атрибутом, имеющим то же имя, что и переменная, которой присваивается дескриптор.
+
+При обращении к атрибуту дескриптор должен возвращать значение этого атрибута, если оно установлено. Если значение атрибута не установлено, должно быть возбуждено исключение AttributeError с текстом:
+
+Атрибут не найден
+При установке или изменении значения атрибута дескриптор должен сперва проверять его на корректность с помощью метода validate().
+
+Класс Validator должен иметь один абстрактный метод экземпляра:
+
+validate() — пустой метод
+2. Также реализуйте класс Number, наследника класса Validator, описывающий дескриптор, который проверяет, что устанавливаемое или изменяемое значение является числом из определенного диапазона. Дескриптор должен закрепляться за атрибутом, имеющим то же имя, что и переменная, которой присваивается дескриптор. При создании экземпляра класс должен принимать два аргумента в следующем порядке:
+
+minvalue — левая граница диапазона, по умолчанию имеет значение None и не ограничивает число слева
+maxvalue — правая граница диапазона, по умолчанию имеет значение None и не ограничивает число справа
+Класс Number должен иметь один метод экземпляра:
+
+validate() — метод, принимающий в качестве аргумента произвольный объект и возбуждающий исключение, если он не удовлетворяет каким-либо условиям. Если указанный объект не принадлежит типу int или float, должно быть возбуждено исключение TypeError с текстом:
+Устанавливаемое значение должно быть числом
+Если указанный объект является числом меньше minvalue, должно быть возбуждено исключение ValueError с текстом:
+Устанавливаемое число должно быть больше или равно <minvalue>
+Если указанный объект является числом больше maxvalue, должно быть возбуждено исключение ValueError с текстом:
+Устанавливаемое число должно быть меньше или равно <maxvalue>
+3. Наконец, реализуйте класс String, наследника класса Validator, описывающий дескриптор, который проверяет, что устанавливаемое или изменяемое значение является строкой определенной длины. Дескриптор должен закрепляться за атрибутом, имеющим то же имя, что и переменная, которой присваивается дескриптор. При создании экземпляра класс должен принимать три аргумента в следующем порядке:
+
+minsize — минимальная длина слова, по умолчанию имеет значение None и не ограничивает минимальную длину
+maxsize — максимальная длина слова, по умолчанию имеет значение None и не ограничивает максимальную длину
+predicate — функция-предикат для дополнительной валидации, по умолчанию имеет значение None и не используется
+Класс String должен иметь один метод экземпляра:
+
+validate() — метод, принимающий в качестве аргумента произвольный объект и возбуждающий исключение, если он не удовлетворяет каким-либо условиям. Если указанный объект не принадлежит типу str, метод должен возбуждать исключение TypeError с сообщением:
+Устанавливаемое значение должно быть строкой
+Если указанный объект является строкой с длиной меньше minsize, должно быть возбуждено исключение ValueError с текстом:
+Длина устанавливаемой строки должна быть больше или равна <minsize>
+Если указанный объект является строкой с длиной больше maxsize, должно быть возбуждено исключение ValueError с текстом:
+Длина устанавливаемой строки должна быть меньше или равна <maxsize>
+Если указанный объект при передаче в функцию predicate() возвращает False, должно быть возбуждено исключение ValueError с текстом:
+Устанавливаемая строка не удовлетворяет дополнительным условиям
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованные классы используется только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализации классов нет, она может быть произвольной.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+class Student:
+    age = Number(18, 99)
+
+
+student = Student()
+student.age = 19
+print(student.age)
+Sample Output 1:
+
+19
+Sample Input 2:
+
+class Student:
+    age = Number(18, 99)
+
+try:
+    student = Student()
+    student.age = '19'
+except TypeError as error:
+    print(error)
+Sample Output 2:
+
+Устанавливаемое значение должно быть числом
+Sample Input 3:
+
+class Student:
+    age = Number(18, 99)
+
+try:
+    student = Student()
+    student.age = 16
+except ValueError as error:
+    print(error)
+Sample Output 3:
+
+Устанавливаемое число должно быть больше или равно 18
+Sample Input 4:
+
+class Student:
+    age = Number(18, 99)
+
+try:
+    student = Student()
+    student.age = 101
+except ValueError as error:
+    print(error)
+Sample Output 4:
+
+Устанавливаемое число должно быть меньше или равно 99
+
+"""
+from abc import ABC, abstractmethod
+
+class Validator(ABC):
+    def __set_name__(self, cls, attr):
+        self.attr = attr
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        if self.attr in obj.__dict__:
+            return obj.__dict__[self.attr]
+        else:
+            raise AttributeError('Атрибут не найден')
+
+    def __set__(self, obj, value):
+        if self.validate(value) is None:
+            obj.__dict__[self.attr] = value
+        else:
+            raise ValueError('Некорректное значение')
+    
+    @abstractmethod 
+    def validate(self, value):
+        pass
+
+class Number(Validator):
+
+    def __init__(self, minvalue=None, maxvalue=None):
+        self.minvalue = float('-inf') if minvalue is None else minvalue
+        self.maxvalue = float('inf') if maxvalue is None else maxvalue
+       
+    def validate(self, value):
+        if type(value) not in (int, float):
+            raise TypeError("Устанавливаемое значение должно быть числом")
+        if value < self.minvalue:
+            raise ValueError(f"Устанавливаемое число должно быть больше или равно {self.minvalue}")
+        if value > self.maxvalue:
+            raise ValueError(f"Устанавливаемое число должно быть меньше или равно {self.maxvalue}")
+
+
+class String(Validator):
+
+    def __init__(self, minsize=None, maxsize=None, predicate=None):
+        self.minsize = float('-inf') if minsize is None else minsize
+        self.maxsize = float('inf') if maxsize is None else maxsize
+        self.predicate = predicate
+
+    def validate(self, value):
+        if type(value) != str:
+            raise TypeError("Устанавливаемое значение должно быть строкой")
+        if len(value) < self.minsize:
+            raise ValueError(f"Длина устанавливаемой строки должна быть больше или равна {self.minsize}")
+        if len(value) > self.maxsize:
+            raise ValueError(f"Длина устанавливаемой строки должна быть меньше или равна {self.maxsize}")
+        if self.predicate is not None and not self.predicate(value):
+            raise ValueError("Устанавливаемая строка не удовлетворяет дополнительным условиям")
+
+
+
 
 
 from collections.abc import *
@@ -391,3 +612,14 @@ if __name__ == '__main__':
     # Используем вложенный итератор для обхода страниц в библиотеке
     for page in library:
         print(page)
+
+
+
+    class Student:
+        age = Number(18, 99)
+
+    try:
+        student = Student()
+        student.age = '19'
+    except TypeError as error:
+        print(error)
