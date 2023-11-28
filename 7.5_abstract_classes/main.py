1fb9014bb97b17db71ddb5124220c25bf62b439c
Gariknik <nikitenkoivi@gmail.com>
2023-11-28 10:00:20 +0200

    solve abstract classes tasks 28/11/2023

--- 7.5_abstract_classes/main.py
+++ 7.5_abstract_classes/main.py
@@ -408,8 +408,101 @@ class String(Validator):
         if self.predicate is not None and not self.predicate(value):
             raise ValueError("Устанавливаемая строка не удовлетворяет дополнительным условиям")
 
+"""
 
+Класс CustomRange
+Назовем диапазоном запись двух целых неотрицательных чисел через дефис a-b, где a — левая граница диапазона, b — правая граница диапазона, причем a <= b. Диапазон содержит в себе все числа от a до b включительно. Например, диапазон 1-4 содержит числа 1, 2, 3 и 4.
 
+Реализуйте класс CustomRange, описывающий последовательность, элементами которой являются одиночные целые числа и числа из определенных диапазонов. При создании экземпляра класс должен принимать произвольное количество позиционных аргументов, каждый из которых является одиночным целым числом либо диапазоном.
+
+При передаче экземпляра класса CustomRange в функцию len() должно возвращаться количество элементов в нем. При передаче экземпляра класса CustomRange в функцию reversed() должен возвращаться итератор, элементами которого являются элементы переданного экземпляра класса CustomRange, расположенные в обратном порядке.
+
+Экземпляр класса CustomRange должен быть итерируемым объектом, то есть позволять перебирать свои элементы, например, с помощью цикла for.
+
+Помимо этого, экземпляр класса CustomRange должен поддерживать операцию проверки на принадлежность с помощью оператора in.
+
+Наконец, экземпляр класса CustomRange должен позволять получать значения своих элементов с помощью индексов, причем как положительных, так и отрицательных
+
+Примечание 1. Перед решением подумайте, какой абстрактный класс из модуля collections.abc будет удобен в качестве родительского.
+
+Примечание 2. Реализация класса CustomRange может быть произвольной, то есть требований к наличию определенных атрибутов нет.
+
+Примечание 3. Дополнительная проверка данных на корректность в методах не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 4. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+customrange = CustomRange(1, '2-5', 5, '6-8')
+
+print(customrange[0])
+print(customrange[1])
+print(customrange[2])
+print(customrange[-1])
+print(customrange[-2])
+print(customrange[-3])
+Sample Output 1:
+
+1
+2
+3
+8
+7
+6
+Sample Input 2:
+
+customrange = CustomRange(1, '2-5', 3, '1-4')
+
+print(*customrange)
+print(*reversed(customrange))
+print(len(customrange))
+print(1 in customrange)
+print(10 in customrange)
+Sample Output 2:
+
+1 2 3 4 5 3 1 2 3 4
+4 3 2 1 3 5 4 3 2 1
+10
+True
+False
+Sample Input 3:
+
+customrange = CustomRange()
+
+print(len(customrange))
+print(*customrange)
+print(*reversed(customrange))
+Sample Output 3:
+
+0
+
+"""
+from collections.abc import Sequence
+
+class CustomRange(Sequence):
+    def __init__(self, *args):
+        mut_iterable = self.mutable_iterable(args)
+        self.iterable = mut_iterable
+    
+    @staticmethod
+    def mutable_iterable(iterable):
+        result = []
+        for n in iterable:
+            if type(n) == str:
+                start, stop = map(int, n.split('-'))
+                stop += 1
+                result += list(range(start, stop))
+            else:
+                result += [n]
+        return result
+    
+    def __getitem__(self, key):
+        return self.iterable[key]
+    
+    def __len__(self):
+        return len(self.iterable)
 
 
 from collections.abc import *
@@ -623,3 +716,14 @@ if __name__ == '__main__':
         student.age = '19'
     except TypeError as error:
         print(error)
+
+
+
+    customrange = CustomRange(1, '2-5', 5, '6-8')
+
+    print(customrange[0])
+    print(customrange[1])
+    print(customrange[2])
+    print(customrange[-1])
+    print(customrange[-2])
+    print(customrange[-3])
