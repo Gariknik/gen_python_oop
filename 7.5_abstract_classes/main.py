2c48224221e115f78781c4d35a282f0e8f741533
Gariknik <nikitenkoivi@gmail.com>
2023-11-30 10:51:47 +0200

    solve abstractionclass tasks 30/11/2023

--- 7.5_abstract_classes/main.py
+++ 7.5_abstract_classes/main.py
@@ -505,6 +505,126 @@ class CustomRange(Sequence):
         return len(self.iterable)
 
 
+"""
+
+Класс BitArray
+Реализуйте класс BitArray, описывающий битовый список, то есть список, элементами которого являются только нули и единицы. При создании экземпляра класс должен принимать один аргумент:
+
+iterable — итерируемый объект, определяющий начальный набор элементов битового списка. Если не передан, начальный набор считается пустым
+Экземпляр класса BitArray должен иметь следующее неформальное строковое представление:
+
+[<первый элемент битового списка>, <второй элемент битового списка>, ...]
+При передаче экземпляра класса BitArray в функцию len() должно возвращаться количество элементов в нем. При передаче экземпляра класса BitArray в функцию reversed() должен возвращаться итератор, элементами которого являются элементы переданного экземпляра класса BitArray , расположенные в обратном порядке.
+
+Экземпляр класса BitArray должен быть итерируемым объектом, то есть позволять перебирать свои элементы, например, с помощью цикла for.
+
+Помимо этого, экземпляр класса BitArray должен поддерживать операцию проверки на принадлежность с помощью оператора in.
+
+Также экземпляр класса BitArray должен позволять получать значения своих элементов с помощью индексов, причем как положительных, так и отрицательных.
+
+Вдобавок ко всему, экземпляр класса BitArray должен поддерживать унарный оператор ~, выполняющий операцию логического отрицания для каждого бита битового списка, тем самым преобразуя 0 в 1, а 1 в 0. Результатом работы оператора должен являться новый экземпляр класса BitArray.
+
+Наконец, экземпляры класса BitArray должны поддерживать между собой логические операции с помощью операторов & и |:
+
+оператор & должен выполнять операцию логического И над каждой парой битов двух битовых списков равной длины. Результатом работы оператора должен являться новый экземпляр класса BitArray
+
+оператор | должен выполнять операцию логического ИЛИ над каждой парой битов двух битовых списков равной длины. Результатом работы оператора должен являться новый экземпляр класса BitArray
+
+Примечание 1. Перед решением подумайте, какой абстрактный класс из модуля collections.abc будет удобен в качестве родительского.
+
+Примечание 2. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 3. Экземпляр класса BitArray не должен зависеть от итерируемого объекта, на основе которого он был создан. Другими словами, если исходный итерируемый объект изменится, то экземпляр класса BitArray измениться  не должен.
+
+Примечание 4. Если объект, с которым выполняется логическая операция, некорректен, метод, реализующий эту операцию, должен вернуть константу NotImplemented.
+
+Примечание 5. Никаких ограничений касательно реализации класса BitArray нет, она может быть произвольной.
+
+Примечание 6. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+bitarray = BitArray([1, 0, 1, 1])
+
+print(bitarray)
+print(~bitarray)
+print(bitarray[0])
+print(bitarray[1])
+print(bitarray[-1])
+print(0 in bitarray)
+print(1 not in bitarray)
+Sample Output 1:
+
+[1, 0, 1, 1]
+[0, 1, 0, 0]
+1
+0
+1
+True
+False
+Sample Input 2:
+
+bitarray1 = BitArray([1, 0, 1, 1])
+bitarray2 = BitArray([0, 0, 0, 1])
+
+bitarray3 = bitarray1 | bitarray2
+bitarray4 = bitarray1 & bitarray2
+bitarray5 = ~bitarray1
+
+print(bitarray3, type(bitarray3))
+print(bitarray4, type(bitarray4))
+print(bitarray5, type(bitarray5))
+Sample Output 2:
+
+[1, 0, 1, 1] <class '__main__.BitArray'>
+[0, 0, 0, 1] <class '__main__.BitArray'>
+[0, 1, 0, 0] <class '__main__.BitArray'>
+
+"""
+
+from collections.abc import Sequence
+
+class BitArray(Sequence):
+    def __init__(self, iterable=None):
+        if iterable is None:
+            self.it = []
+            self.iterable = self.it[:]
+        else:
+            self.it = iterable
+            self.iterable = self.it[:]
+
+    def __str__(self):
+        return f"[{', '.join([str(n) for n in self.iterable])}]"
+
+    def __getitem__(self, key):
+        return self.iterable[key]
+    
+    def __iter__(self):
+        return iter(self.iterable)
+    
+    def __len__(self):
+        return len(self.iterable)
+    
+    def reversed(self):
+        return self.iterable[::-1]
+    
+    def __invert__(self):
+        return BitArray([n-1 if n == 1 else n+1 for n in self.iterable])
+    
+    def __and__(self, other):
+        if isinstance(other, BitArray):
+            return BitArray([n1 and n2 for n1, n2 in zip(self.iterable, other.iterable)])
+        return NotImplemented
+    
+    def __or__(self, other):
+        if isinstance(other, BitArray):
+            return BitArray([n1 or n2 for n1, n2 in zip(self.iterable, other.iterable)])
+        return NotImplemented
+
+
+
 from collections.abc import *
 print(isinstance([1, 2, 3], Iterable))
 print(isinstance([1, 2, 3], Iterator))
@@ -727,3 +847,14 @@ if __name__ == '__main__':
     print(customrange[-1])
     print(customrange[-2])
     print(customrange[-3])
+
+    data = [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0,
+            0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0]
+
+    bitarray = BitArray(data)
+
+    print(bitarray)
+    data.extend([0, 0, 1, 0, 1, 1])
+
+    print(data)
+    print(bitarray)
