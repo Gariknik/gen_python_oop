28b5c15d82a538e9d5c59e4cfc38d592187e2448
Gariknik <nikitenkoivi@gmail.com>
2023-12-01 10:01:35 +0200

    solve abstracion classes tasks 01/12/2023

--- 7.5_abstract_classes/main.py
+++ 7.5_abstract_classes/main.py
@@ -622,7 +622,150 @@ class BitArray(Sequence):
         if isinstance(other, BitArray):
             return BitArray([n1 or n2 for n1, n2 in zip(self.iterable, other.iterable)])
         return NotImplemented
+    
 
+"""
+Класс DNA
+ДНК состоит из двух цепей, ориентированных азотистыми основаниями друг к другу. В ДНК встречается четыре вида азотистых оснований: аденин (A), гуанин (G), тимин (T) и цитозин (C). Азотистые основания одной из цепей соединены с азотистыми основаниями другой цепи водородными связями согласно принципу комплементарности: аденин (A) соединяется только с тимином (T), гуанин (G) — только с цитозином (C).
+
+
+
+Реализуйте класс DNA, описывающий двухцепочную спираль ДНК. При создании экземпляра класс должен принимать один аргумент:
+
+chain — первая цепь ДНК, представляющая собой строку из символов A, G, T и C (азотистых оснований)
+Экземпляр класса DNA должен иметь следующее неформальное строковое представление:
+
+<азотистые основания первой цепи ДНК>
+При передаче экземпляра класса DNA в функцию len() должно возвращаться количество азотистых оснований в одной из его цепей. При передаче экземпляра класса в функцию reversed() должен возвращаться итератор, элементами которого являются элементы переданного экземпляра класса DNA, расположенные в обратном порядке.
+
+Помимо этого, экземпляр класса DNA должен быть итерируемым объектом, то есть позволять перебирать свои элементы, например, с помощью цикла for.
+
+Также экземпляр класса DNA должен позволять получать значения своих элементов с помощью индексов, причем как положительных, так и отрицательных.
+
+В случае с функцией reversed(), итерацией и доступу по индексам элементы экземпляра класса DNA должны быть представлены в виде кортежей из двух элементов, первым из которых является основание первой цепи ДНК по указанному индексу, вторым — азотистое основание второй цепи ДНК по указанному индексу. Азотистое основание второй цепи ДНК можно получить при помощи принципа комплементарности.
+
+Вдобавок ко всему, экземпляр класса DNA должен поддерживать операцию проверки на принадлежность с помощью оператора in. В данном случае должно проверяться, входит ли азотистое основание в первую цепь ДНК.
+
+Экземпляры класса DNA должны поддерживать между собой операции сравнения с помощью операторов == и!=. Две ДНК считаются равными, если их первые цепи совпадают.
+
+Наконец, экземпляры класса DNA должны поддерживать между собой операцию сложения с помощью оператора +, результатом которой должен являться новый экземпляр класса DNA, первой цепью которого является конкатенация первых цепей исходных экземпляров класса DNA.
+
+Примечание 1. Перед решением подумайте, какой абстрактный класс из модуля collections.abc будет удобен в качестве родительского.
+
+Примечание 2. Если объект, с которым выполняется операция сравнения или сложения, некорректен, метод, реализующий эту операцию, должен вернуть константу NotImplemented.
+
+Примечание 3. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 4. Никаких ограничений касательно реализации класса DNA нет, она может быть произвольной.
+
+Примечание 5. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+dna = DNA('AGTC')
+
+print(dna[0])
+print(dna[1])
+print(dna[2])
+print(dna[3])
+print(dna[-1])
+print(dna[-2])
+Sample Output 1:
+
+('A', 'T')
+('G', 'C')
+('T', 'A')
+('C', 'G')
+('C', 'G')
+('T', 'A')
+Sample Input 2:
+
+dna = DNA('AGT')
+
+print(dna)
+print(len(dna))
+print('A' in dna)
+print('C' in dna)
+Sample Output 2:
+
+AGT
+3
+True
+False
+Sample Input 3:
+
+dna1 = DNA('ACG')
+dna2 = DNA('TTTAAT')
+dna3 = dna1 + dna2
+dna4 = dna2 + dna1
+
+print(dna3, type(dna3))
+print(dna4, type(dna4))
+Sample Output 3:
+
+ACGTTTAAT <class '__main__.DNA'>
+TTTAATACG <class '__main__.DNA'>
+Sample Input 4:
+
+dna1 = DNA('ACG')
+dna2 = DNA('TTTAAT')
+dna3 = DNA('TTTAAT')
+
+print(dna1 == dna2)
+print(dna2 == dna3)
+print(dna1 != dna3)
+print(dna2 != dna3)
+Sample Output 4:
+
+False
+True
+True
+False
+
+
+"""
+from collections.abc import Sequence
+
+class DNA(Sequence):
+    __BASE = {'A': 'T', 'G': 'C', 'T': 'A', 'C': 'G'}
+    def __init__(self, chain):
+        self.chain = chain
+
+    def __str__(self):
+        return self.chain
+
+    def __getitem__(self, key):
+        return (self.chain[key], self.__BASE[self.chain[key]])
+    
+    def __iter__(self):
+        return iter([(ch, self.__BASE[ch]) for ch in self.chain])
+    
+    def __len__(self):
+        return len(self.chain)
+    
+    def reversed(self):
+        return self.chain[::-1]
+    
+    def __contains__(self, value):
+        return value in self.chain
+    
+    def __eq__(self, other):
+        if isinstance(other, DNA):
+            return all([ch1 == ch2 for ch1, ch2 in zip(self.chain, other.chain)])
+        return NotImplemented
+    
+    def __add__(self, other):
+        if isinstance(other, DNA):
+            return DNA(self.chain + other.chain)
+        return NotImplemented
+    
+    def __radd__(self, other):
+        if isinstance(other, DNA):
+            return DNA(other.chain + self.chain)
+        return NotImplemented
+        
 
 
 from collections.abc import *
@@ -858,3 +1001,11 @@ if __name__ == '__main__':
 
     print(data)
     print(bitarray)
+
+    dna1 = DNA('ACG')
+    dna2 = DNA('TTTAAT')
+    dna3 = dna1 + dna2
+    dna4 = dna2 + dna1
+
+    print(dna3, type(dna3))
+    print(dna4, type(dna4))
