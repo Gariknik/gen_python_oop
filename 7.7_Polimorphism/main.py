1803607fb0d36e561d805772c053965ee49c5826
Gariknik <nikitenkoivi@gmail.com>
2023-12-22 14:00:30 +0200

    solve polimorphism tasks 22/12/2023

--- 7.7_Polimorphism/main.py
+++ 7.7_Polimorphism/main.py
@@ -152,10 +152,123 @@ class AverageStat(Stat):
     def result(self):
         if self:
             return sum(self)/len(self)
+        
+"""
+Классы LeftParagraph и RightParagraph
+Будем называть словом любую последовательность из одной или более латинских букв.
+
+1. Реализуйте класс LeftParagraph, описывающий абзац, выровненный по левому краю. При создании экземпляра класс должен принимать один аргумент:
+
+length — длина строки абзаца
+Класс LeftParagraph должен иметь два метода экземпляра:
+
+add() — метод, принимающий в качестве аргумента слово или несколько слов, разделенных пробелом, и добавляющий их в текущий абзац. Если слово не помещается на текущей строке, оно переносится на следующую. Также метод должен автоматически добавлять один пробел после каждого добавленного слова (кроме последнего)
+end() — метод, печатающий текущий абзац, выровненный по левому краю. После вызова метода текущий абзац считается пустым, то есть начинается формирование нового
+2. Также реализуйте класс RightParagraph, описывающий абзац, выровненный по правому краю. При создании экземпляра класс должен принимать один аргумент:
+
+length — длина строки абзаца
+Класс RightParagraph должен иметь два метода экземпляра:
+
+add() — метод, принимающий в качестве аргумента слово или несколько слов, разделенных пробелом, и добавляющий их в текущий абзац. Если слово не помещается на текущей строке, оно переносится на следующую. Также метод должен автоматически добавлять один пробел после каждого добавленного слова (кроме последнего)
+end() — метод, печатающий текущий абзац, выровненный по правому краю с учетом длины строки. После вызова метода текущий абзац считается пустым, то есть начинается формирование нового
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованные классы используются только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализаций классов нет, они могут быть произвольными.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+leftparagraph = LeftParagraph(10)
+
+leftparagraph.add('death')
+leftparagraph.add('can have me')
+leftparagraph.add('when it earns me')
+leftparagraph.end()
+Sample Output 1:
+
+death can
+have me
+when it
+earns me
+Sample Input 2:
+
+rightparagraph = RightParagraph(10)
+
+rightparagraph.add('death')
+rightparagraph.add('can have me')
+rightparagraph.add('when it earns me')
+rightparagraph.end()
+Sample Output 2:
+
+ death can
+   have me
+   when it
+  earns me
+
+"""
+
+from abc import ABC, abstractmethod
+
+class AbstractString(ABC):
+    def __init__(self, length):
+        self.length = length
+        self.string = []
+
+    def add(self, string):
+        self.string += string.split()
+
+    
+    def __str__(self):
+        return self.string
+
+    @abstractmethod
+    def end(self):
+        pass
+    
 
 
+class LeftParagraph(AbstractString):
 
+    def end(self):
+        result = []
+        last = ''
+        row = ''
+        for i in range(len(self.string)):
+            if not last: 
+                if len(row + self.string[i] + ' ') <= self.length:
+                    row += self.string[i] + ' '
+                else:
+                    last = self.string[i]
+                    result.append(row.strip())
+                    row = ''
+            else:
+                row += last + ' ' + self.string[i] + ' '
+                last = ''
+        print('\n'.join(result))
 
+        
+            
+
+class RightParagraph(AbstractString):
+    def end(self):
+        result = []
+        last = ''
+        row = ''
+        for i in range(len(self.string)):
+            if not last: 
+                if len(row + self.string[i] + ' ') <= self.length:
+                    row += self.string[i] + ' '
+                else:
+                    last = self.string[i]
+                    result.append(row.strip())
+                    row = ''
+            else:
+                row += last + ' ' + self.string[i] + ' '
+                last = ''
+        [print(r.rjust(self.length, ' ')) for r in result]
 
 
 
@@ -179,3 +292,11 @@ if __name__ == "__main__":
     print(minstat.result())
     print(maxstat.result())
     print(averagestat.result())
+
+    leftparagraph = RightParagraph(23)
+
+    leftparagraph.add('Multiply noise and joy')
+    leftparagraph.add('Sing songs in a good hour')
+    leftparagraph.add('Friendship grace and youth')
+    leftparagraph.add('Our birthday girls')
+    leftparagraph.end()
