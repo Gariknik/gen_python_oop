26bf7c787f2d1abe8833a5d7d1d74af3781c9689
Gariknik <nikitenkoivi@gmail.com>
2023-12-25 12:57:27 +0200

    solve composition tasks 25/12/2023

--- 7.8_Composition/main.py
+++ 7.8_Composition/main.py
@@ -146,6 +146,239 @@ class ShoppingCart:
     def remove(self, name):
         self.items = list(filter(lambda x: x.name != name, self.items))
 
+"""
+
+Классы Card и Deck
+1. Реализуйте класс Card, описывающий игральную карту. При создании экземпляра класс должен принимать два аргумента в следующем порядке:
+
+suit — масть игральной карты, представленная одним из следующих символов:
+♣, ♢, ♡, ♠
+rank — ранг игральной карты, представленный одним из следующих символов или парой символов:
+2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+Экземпляр класса Card должен иметь следующее неформальное строковое представление:
+
+<масть игральной карты><ранг игральной карты>
+2. Также реализуйте класс данных Deck, описывающий классическую колоду из 52 игральных карт. Карты в колоде должны быть расположены сперва в порядке возрастания мастей, а затем — в порядке возрастания рангов. При создании экземпляра класс не должен принимать никаких аргументов.
+
+Класс Deck должен иметь два метода экземпляра:
+
+shuffle() — метод, перемешивающий все карты в колоде. Перемешивать колоду можно только в том случае, если в колоде на данный момент находятся все 52 карты. Если в колоде меньше 52 карт, должно быть возбуждено исключение ValueError с текстом:
+Перемешивать можно только полную колоду
+deal() — метод, удаляющий из колоды последнюю карту и возвращающий ее. Если колода пуста, должно быть возбуждено исключение ValueError с текстом:
+Все карты разыграны
+Экземпляр класса Deck должен иметь следующее неформальное строковое представление:
+
+Карт в колоде: <текущее количество карт в колоде>
+Примечание 1. Порядок старшинства карточных рангов от младшего к старшему:
+
+2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+Порядок старшинства карточных мастей от младшего к старшему:
+
+♣, ♢, ♡, ♠
+Примечание 2. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованные классы используются только с корректными данными.
+
+Примечание 3. Никаких ограничений касательно реализаций классов нет, они могут быть произвольными.
+
+Примечание 4. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+print(Card('♣', '4'))
+print(Card('♡', 'A'))
+print(Card('♢', '10'))
+Sample Output 1:
+
+♣4
+♡A
+♢10
+Sample Input 2:
+
+deck = Deck()
+
+print(deck)
+print(deck.deal())
+print(deck.deal())
+print(deck.deal())
+print(type(deck.deal()))
+print(deck)
+Sample Output 2:
+
+Карт в колоде: 52
+♠A
+♠K
+♠Q
+<class '__main__.Card'>
+Карт в колоде: 48
+Sample Input 3:
+
+deck = Deck()
+
+for _ in range(52):
+    deck.deal()
+    
+try:
+    deck.deal()
+except ValueError as error:
+    print(error)
+Sample Output 3:
+
+Все карты разыграны
+Sample Input 4:
+
+deck = Deck()
+
+deck.deal()
+    
+try:
+    deck.shuffle()
+except ValueError as error:
+    print(error)
+Sample Output 4:
+
+Перемешивать можно только полную колоду
+
+"""
+from random import shuffle
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f'{self.suit}{self.rank}'
+    
+class Deck:
+    def __init__(self):
+        self.deck = [Card(s, r+'0') if r == '1' else Card(s, r) for s in '♣♢♡♠' for r in ('234567891JQKA')]
+
+    def __str__(self):
+        return f'Карт в колоде: {len(self.deck)}'
+    
+    def shuffle(self):
+        if len(self.deck) < 52:
+            raise ValueError('Перемешивать можно только полную колоду')
+        shuffle(self.deck)
+
+    def deal(self):
+        if len(self.deck) < 1:
+            raise ValueError ('Все карты разыграны')
+        return self.deck.pop()
+    
+
+"""
+Класс Queue
+Реализуйте класс Queue, описывающий очередь, элементами которой являются пары ключ: значение. При создании экземпляра класс должен принимать один аргумент:
+
+pairs — список или словарь, определяющий начальный набор элементов очереди. Порядок элементов в очереди должен совпадать с их порядком в переданном итерируемом объекте. Если не передан, очередь считается пустой
+Класс Queue должен иметь два метода экземпляра:
+
+add() — метод, принимающий в качестве аргумента элемент и добавляющий его в конец очереди. Элементом в данном случае является двухэлементный кортеж, содержащий ключ и значение. Если в очереди уже содержится элемент с указанным ключом, он должен быть перенесен в конец очереди, а его значение должно быть обновлено
+pop() — метод, удаляющий из очереди первый элемент и возвращающий его. Элементом в данном случае является двухэлементный кортеж, содержащий ключ и значение. Если очередь пуста, должно быть возбуждено исключение KeyError с текстом:
+Очередь пуста
+Экземпляр класса Queue должен иметь следующее формальное строковое представление:
+
+Queue([(<ключ 1-го элемента>, <значение 1-го элемента>), (<ключ 2-го элемента>, <значение 2-го элемента>), ...])
+При передаче экземпляра класса Queue в функцию len() должно возвращаться количество элементов в нем.
+
+Примечание 1. Вероятно, при решении задачи будет удобно воспользоваться одним из классов из модуля collections.
+
+Примечание 2. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 3. Никаких ограничений касательно реализации класса Queue нет, она может быть произвольной.
+
+Примечание 4. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+queue = Queue()
+
+queue.add(('one', 1))
+queue.add(('two', 2))
+print(queue)
+Sample Output 1:
+
+Queue([('one', 1), ('two', 2)])
+Sample Input 2:
+
+queue = Queue([('one', 1)])
+
+queue.add(('two', 2))
+print(queue.pop())
+print(queue.pop())
+print(queue)
+Sample Output 2:
+
+('one', 1)
+('two', 2)
+Queue([])
+Sample Input 3:
+
+queue = Queue({'one': 1, 'two': 2})
+
+print(len(queue))
+queue.add(('three', 1))
+print(len(queue))
+Sample Output 3:
+
+2
+3
+Sample Input 4:
+
+queue = Queue()
+
+queue.add(('one', 1))
+queue.add(('two', 2))
+print(queue)
+queue.add(('one', 10))
+print(queue)
+Sample Output 4:
+
+Queue([('one', 1), ('two', 2)])
+Queue([('two', 2), ('one', 10)])
+Sample Input 5:
+
+queue = Queue()
+
+try:
+    queue.pop()
+except KeyError as error:
+    print(error)
+Sample Output 5:
+
+'Очередь пуста'
+"""
+class Queue:
+    def __init__(self, pairs=None):
+        if pairs is None:
+            self.pairs = {}
+        else:
+            if type(pairs) == dict:
+                self.pairs = pairs
+            else:
+                self.pairs = {key: value for key, value in pairs}
+                
+    def __str__(self):
+        return f'Queue({[(key, value) for key, value in self.pairs.items()]})'
+    
+    def add(self, pair):
+        if pair[0] in self.pairs:
+            self.pairs.pop(pair[0])
+            self.pairs |= dict([pair])
+        else: 
+            self.pairs |= dict([pair])
+
+    def pop(self):
+        if self.pairs:
+            return (list(self.pairs.keys())[0], self.pairs.pop(list(self.pairs.keys())[0]))
+        raise KeyError ('Очередь пуста')
+    def __len__(self):
+        return len(self.pairs)
+
+
+
 if __name__ == "__main__":
     point = Point(1, 1)
     circle = Circle(5, point)
@@ -159,3 +392,16 @@ if __name__ == "__main__":
 
     print(item1)
     print(item2)
+
+    deck = Deck()
+
+    for _ in range(52):
+        print(deck.deal())
+
+    queue = Queue()
+
+    queue.add(('one', 1))
+    queue.add(('two', 2))
+    print(queue)
+    queue.add(('one', 10))
+    print(queue)
