52f0062e2f266510371b4e30c0d517c4ccefa341
Gariknik <nikitenkoivi@gmail.com>
2024-01-05 13:38:38 +0200

    solve Enum tasks 05/01/2024

--- 8.2_Enum/main.py
+++ 8.2_Enum/main.py
@@ -103,8 +103,120 @@ class Seasons(Enum):
                         4: {'en': 'fall', 'ru': 'осень'}}
         return dict_seasons[self.value][l]
 
+"""
+Классы Weekday и NextDate
+1. Реализуйте класс Weekday, описывающий перечисление с днями недели. Перечисление должно иметь семь элементов:
 
+MONDAY — элемент со значением 0
+TUESDAY — элемент со значением 1
+WEDNESDAY — элемент со значением 2
+THURSDAY — элемент со значением 3
+FRIDAY — элемент со значением 4
+SATURDAY — элемент со значением 5
+SUNDAY — элемент со значением 6
+2. Также реализуйте класс NextDate, позволяющий определять дату следующего дня недели, начиная с текущего дня. При создании экземпляра класс должен принимать три аргумента в следующем порядке:
+
+today — дата текущего дня, представленная экземпляром класса date
+weekday — день недели, представленный элементом перечисления Weekday
+after_today — булево значение, по умолчанию равняется False
+Параметр after_today должен определять, учитывается ли текущая дата при определении даты следующего дня недели. Если он имеет значение False, текущая дата не должна учитываться, если True — должна учитываться.
+
+Класс NextDate должен иметь два метода экземпляра:
+
+date() — метод, возвращающий дату следующего дня недели в виде экземпляра класса date
+days_until() — метод, возвращающий количество дней до даты следующего дня недели
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованные классы используются только с корректными данными.
+
+Примечание 2. Никаких ограничений касательно реализаций классов нет, они могут быть произвольными.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+Sample Input 1:
+
+from datetime import date
+
+today = date(2023, 4, 17)                              # понедельник
+next_friday = NextDate(today, Weekday.FRIDAY)          # следующая пятница
+
+print(next_friday.date())
+print(next_friday.days_until())
+Sample Output 1:
+
+2023-04-21
+4
+Sample Input 2:
+
+from datetime import date
+
+today = date(2023, 4, 17)                              # понедельник
+next_monday = NextDate(today, Weekday.MONDAY)          # следующий понедельник без учета текущего
+
+print(next_monday.date())
+print(next_monday.days_until())
+Sample Output 2:
+
+2023-04-24
+7
+Sample Input 3:
+
+from datetime import date
+
+today = date(2023, 4, 17)                              # понедельник
+next_monday = NextDate(today, Weekday.MONDAY, True)    # следующий понедельник с учетом текущего
+
+print(next_monday.date())
+print(next_monday.days_until())
+Sample Output 3:
+
+2023-04-17
+0
+
+"""
+
+from enum import Enum
+from datetime import datetime, timedelta
+
+
+class Weekday(Enum):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
+
+class NextDate:
+    def __init__(self, today, weekday, after_today=False) -> None:
+        self.count = 0
+        self.today = datetime(today.year, today.month, today.day)
+        self.weekday = weekday
+        self.after_today = after_today
+
+    def date(self):
+        if self.today.date().weekday() == self.weekday.value and self.after_today == True:
+            return self.today.date()
+        if self.today.date().weekday() == self.weekday.value and self.after_today == False:
+            self.today += timedelta(days=1)
+            self.count += 1
+            self.today, self.count = self.find_next_day(self.today, self.weekday, self.count)
+            return self.today.date()
+        if self.today.date().weekday() != self.weekday.value:
+            self.today, self.count = self.find_next_day(self.today, self.weekday, self.count)
+            return self.today.date()
+    @staticmethod
+    def find_next_day(today, weekday, count):
+        while today.date().weekday() != weekday.value:
+            today += timedelta(days=1)
+            count += 1
+        return today, count
         
+    def days_until(self):
+        return self.count
+
+
 
 if __name__ == "__main__":
     print(HTTPStatusCodes.OK.info())
@@ -115,3 +227,13 @@ if __name__ == "__main__":
 
     print(Seasons.FALL.text_value('ru'))
     print(Seasons.FALL.text_value('en'))
+
+
+    from datetime import date
+
+    for weekday in Weekday:
+        today = date(2023, 4, 27)                              # четверг
+        next_date = NextDate(today, weekday, True)
+
+        print(next_date.date())
+        print(next_date.days_until())
