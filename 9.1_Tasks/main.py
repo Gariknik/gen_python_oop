fa56158d01abe54fbc531ca911b304192f5f03e9
Gariknik <nikitenkoivi@gmail.com>
2024-01-25 09:29:51 +0200

    solve Tasks 25/01/2024

--- 9.1_Tasks/main.py
+++ 9.1_Tasks/main.py
@@ -170,8 +170,51 @@ class Domain:
 
 
 
+"""
+Класс HighScoreTable
+Предположим, что у нас имеется некоторая игра. За каждую игровую сессию игрок получает определенное количество баллов в зависимости от своего результата. Вашей задачей является реализация класса HighScoreTable — таблицы рекордов, которую можно будет обновлять с учетом итоговых результатов игрока.
+
+Изначально таблица рекордов является пустой. Максимальное количество рекордов указывается при создании таблицы:
+
+high_score_table = HighScoreTable(3)
+Таблица должна позволять просматривать текущие рекорды и добавлять новые результаты:
+
+print(high_score_table.scores)    # []
+high_score_table.update(10)
+high_score_table.update(8)
+high_score_table.update(12)
+print(high_score_table.scores)    # [12, 10, 8]
+Текущие рекорды всегда должны располагаться в порядке убывания. Так как таблица содержит именно рекорды, то после заполнения таблицы добавляться должны только те результаты, которые лучше текущих:
+
+high_score_table.update(6)
+high_score_table.update(7)
+print(high_score_table.scores)    # [12, 10, 8]
+high_score_table.update(9)
+print(high_score_table.scores)    # [12, 10, 9]
+Таблица должна поддерживать сброс всех результатов:
+
+high_score_table.reset()
+print(high_score_table.scores)    # []
+Примечание. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+GitHub
+
+"""
+
+class HighScoreTable:
+    def __init__(self, limit):
+        self.limit = limit
+        self.scores = []
+
+    def update(self, score):
+        self.scores = sorted(self.scores + [score], reverse=True)[:self.limit]
+
+    def reset(self):
+        self.scores = []
 
 
+    
 
 
 
@@ -217,3 +260,18 @@ if __name__ == '__main__':
     print(type(domain1))
     print(type(domain2))
     print(type(domain3))
+
+
+
+    high_score_table = HighScoreTable(3)
+
+    print(high_score_table.scores)
+    high_score_table.update(10)
+    high_score_table.update(8)
+    high_score_table.update(12)
+    print(high_score_table.scores)
+
+    high_score_table.update(18)
+    high_score_table.update(11)
+    high_score_table.update(13)
+    print(high_score_table.scores)
